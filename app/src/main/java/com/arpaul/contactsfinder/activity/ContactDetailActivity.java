41db602ce352e752a2d39e51d101f1d727f21db5
Aritra1704 <aritra1704@gmail.com>
2016-08-24 12:19:55 +0530

    Modified detail screen.

--- app/src/main/java/com/arpaul/contactsfinder/activity/ContactDetailActivity.java
+++ app/src/main/java/com/arpaul/contactsfinder/activity/ContactDetailActivity.java
@@ -8,14 +8,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.arpaul.contactsfinder.R;
+import com.arpaul.contactslibrary.dataObjects.AddressDO;
 import com.arpaul.contactslibrary.dataObjects.ContactsDO;
+import com.arpaul.contactslibrary.dataObjects.EmailIdDO;
 import com.arpaul.utilitieslib.UnCaughtException;
+
+import java.util.ArrayList;
 
 public class ContactDetailActivity extends AppCompatActivity {
 
     private ContactsDO objContactsDO;
+
+    private Toolbar toolbar;
+    private TextView tvContactName, tvContactNumber, tvContactEmail, tvContactNotes, tvContactAddress, tvContactBday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +34,49 @@ public class ContactDetailActivity extends AppCompatActivity {
         if(getIntent().hasExtra("CONTACT_DETAIL"))
             objContactsDO = (ContactsDO) getIntent().getExtras().get("CONTACT_DETAIL");
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        initialiseControls();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        bindControls();
+    }
+
+    private void bindControls(){
+        tvContactName.setText((String) objContactsDO.getData(ContactsDO.DATA_CONTACT.CONTACT_NAME));
+
+        ArrayList<String> arrContactNumber = (ArrayList<String>) objContactsDO.getData(ContactsDO.DATA_CONTACT.CONTACT_NUMBER);
+        StringBuilder contacts = new StringBuilder();
+        for(String contact : arrContactNumber){
+            contacts.append(contact).append("\n");
+        }
+        tvContactNumber.setText(contacts.toString());
+
+        ArrayList<EmailIdDO> arrEmail = (ArrayList<EmailIdDO>) objContactsDO.getData(ContactsDO.DATA_CONTACT.CONTACT_EMAIL);
+        StringBuilder emails = new StringBuilder();
+        for(EmailIdDO email : arrEmail){
+            emails.append((String) email.getData(EmailIdDO.DATA_EMAIL.EMAIL_ID)).append("\n");
+        }
+        tvContactEmail.setText(emails.toString());
+
+        tvContactNotes.setText((String) objContactsDO.getData(ContactsDO.DATA_CONTACT.CONTACT_NOTES));
+
+        ArrayList<AddressDO> arrAddress = (ArrayList<AddressDO>) objContactsDO.getData(ContactsDO.DATA_CONTACT.CONTACT_ADDRESS);
+        StringBuilder address = new StringBuilder();
+        for(AddressDO contact : arrAddress){
+            address.append((String) contact.getData(AddressDO.DATA_ADDRESS.ADDRESS_POBOX)).append(" ");
+            address.append((String) contact.getData(AddressDO.DATA_ADDRESS.ADDRESS_STREET)).append(" ");
+            address.append((String) contact.getData(AddressDO.DATA_ADDRESS.ADDRESS_CITY)).append(" ");
+            address.append((String) contact.getData(AddressDO.DATA_ADDRESS.ADDRESS_STATE)).append(" ");
+            address.append((String) contact.getData(AddressDO.DATA_ADDRESS.ADDRESS_POSTCODE)).append(" ");
+            address.append((String) contact.getData(AddressDO.DATA_ADDRESS.ADDRESS_COUNTRY)).append("\n");
+        }
+        tvContactAddress.setText(address.toString());
+
+        ArrayList<String> arrBirthday = (ArrayList<String>) objContactsDO.getData(ContactsDO.DATA_CONTACT.CONTACT_BDAY);
+        StringBuilder bdays = new StringBuilder();
+        for(String bday : arrBirthday){
+            bdays.append(bday).append("\n");
+        }
+        tvContactBday.setText(bdays.toString());
+
     }
 
     @Override
@@ -58,5 +98,18 @@ public class ContactDetailActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initialiseControls(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tvContactName       = (TextView) findViewById(R.id.tvContactName);
+        tvContactNumber     = (TextView) findViewById(R.id.tvContactNumber);
+        tvContactEmail      = (TextView) findViewById(R.id.tvContactEmail);
+        tvContactNotes      = (TextView) findViewById(R.id.tvContactNotes);
+        tvContactAddress    = (TextView) findViewById(R.id.tvContactAddress);
+        tvContactBday       = (TextView) findViewById(R.id.tvContactBday);
+
     }
 }
